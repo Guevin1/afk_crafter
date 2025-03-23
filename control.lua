62306f51bd1a00e0ddbadfdd4f85c05996b31150
guevin1 <eis01082008@gmail.com>
2025-03-23 21:33:28 +0300

    add crafting

--- control.lua
+++ control.lua
@@ -1,9 +1,21 @@
 local playerTable = require("playerTable")
 commands.add_command("testRecipe",nil, function (p1)
     playerTable.addRecipe(1,"iron-gear-wheel",10)
+    playerTable.addRecipe(1,"copper-cable",10,0,10)
+    playerTable.addRecipe(1,"iron-stick",20,10)
+    playerTable.addRecipe(1,"uranium-processing",5,10)
+    local recipeID = playerTable.addRecipe(1,"inserter",20)
+    playerTable.addNeeded(1,recipeID or 5,"electronic-circuit","!=",0)
+    playerTable.sortRecipes(1)
+
 end)
 commands.add_command("break",nil, function (p1)
     print("Break")
+end)
+commands.add_command("resetafkc", nil, function (p1)
+    playerTable.initTables()
+    initModPlayers()
+    game.print("reset AFK crafter! ")
 end)
 function initModPlayers()
     if not playerTable.isTableInit() then
@@ -17,6 +29,66 @@ function initModPlayers()
         end
     end
 end
+function crafting()
+    
+    for _, player in pairs(game.players) do
+        local playerInfo = playerTable.getPlayer(player.index)
+        local crafting_queue = player.crafting_queue
+        if playerInfo["active"] > 0 and player.character ~= nil and (crafting_queue == nil or #crafting_queue == 0) then
+            local group = playerTable.getGroup(playerInfo["active"])
+            if (#group["surfaces"] == 0 or table.contains(group["surfaces"],player.surface_index)) then
+                if (#group["recipes"] > 0) then
+                    for _, recipe in pairs(group["recipes"]) do
+                        local createRecipe = recipe["enabled"]
+                        local nameRecipe = recipe["name"]
+                        local force = player.force
+                        local recipeForce = force.recipes[nameRecipe]
+                        local product = nameRecipe
+                        if recipeForce then
+                            product = recipeForce.products[recipe["productID"]].name
+                        end
+                        local craftWithNeed = true
+                        if #recipe["needed"] > 0 then
+                            for _, need in pairs(recipe["needed"]) do
+                                itemsCount = player.get_item_count(need["name"])
+                                if not playerTable.equals2var(itemsCount,need["type"], need["count"]) then
+                                    craftWithNeed = false
+                                end
+                            end
+                        end
+                        
+
+                        local countsInInventory = player.get_item_count(product)
+                        local rsRecipe = recipe["rs"]
+                        local craftRecipe = countsInInventory < rsRecipe["max"]
+                        if rsRecipe["min"] > 0 then
+                            if rsRecipe["min"] >= countsInInventory then
+                                rsRecipe["enabled"] = true
+                            end
+                            if rsRecipe["max"] <= countsInInventory then
+                                rsRecipe["enabled"] = false
+                            end
+                            craftRecipe = rsRecipe["enabled"]
+                        end
+                        
+                        if craftRecipe and createRecipe and craftWithNeed then
+                            local craftSize = 1
+                            local itemCountCraft = player.begin_crafting{count=craftSize,recipe=nameRecipe,silent=true}
+                            if itemCountCraft ~= 0 then
+                                break
+                            end
+
+                        end
+                    end                   
+                end
+            end 
+        end
+    end
+   
+end
+
+
+script.on_nth_tick(10,crafting)
 script.on_event(defines.events.on_player_joined_game, initModPlayers)
 script.on_init(initModPlayers)
 script.on_configuration_changed(initModPlayers)
--- playerTable.lua
+++ playerTable.lua
@@ -20,45 +20,62 @@ function functions.getPlayerGroup()
 end
 
 function functions.getGroup(id)
-    group = functions.getTableGroup()[id]
+    local group = functions.getTableGroup()[id]
     if group ~= nil then
         return group
     end
 end
 
 function functions.getRecipe(groupID, recipeID)
-    group = functions.getGroup(groupID)
+    local group = functions.getGroup(groupID)
     if group ~= nil then
-        return group[recipeID]
+        return group["recipes"][recipeID]
     end
 end
 
 function functions.getPlayer(idPlayer)
-    players = functions.getPlayerGroup()
+    local players = functions.getPlayerGroup()
     if players ~= nil then
         return players[idPlayer]
     end
 end
 
-function functions.isGroupinPlayer(idPlayer,idGroup)
-    playerTable = functions.getPlayer(idPlayer)
-    if playerTable ~= nil then
-        return table.contains(playerTable,idGroup)
-    else
-        return false
-    end
-end
 function functions.addPlayer(idPlayer)
-    playerGroup = functions.getPlayerGroup()
-    if playerGroup ~= nil and playerGroup[idPlayer] == nil then
-        playerGroup[idPlayer] = {}
+    local playerInfo = functions.getPlayer(idPlayer)
+    if playerInfo == nil then
+        local playerGroup = functions.getPlayerGroup()
+        playerGroup[idPlayer] = {
+            active = 0,
+            groups = {}
+        }
     end 
 end
+
+function functions.getGroupsByPlayer(idPlayer)
+    local playerInfo = functions.getPlayer(idPlayer)
+    if playerInfo ~= nil then
+        return playerInfo["groups"]
+    end
+end
+
+function functions.isGroupinPlayer(idPlayer,idGroup)
+    local playerGroup = functions.getGroupsByPlayer(idPlayer)
+    if playerGroup ~= nil then
+        return table.contains(playerGroup, idGroup)
+    end
+end
+
 function functions.addPlayerInGroup(idPlayer, idGroup)
-    print(idGroup,idPlayer)
-    if functions.getPlayer(idPlayer) ~= nil and not functions.isGroupinPlayer(idPlayer,idGroup) then
-        playerGroup = functions.getPlayer(idPlayer)
+    local playerGroup = functions.getGroupsByPlayer(idPlayer)
+    if playerGroup ~= nil and not functions.isGroupinPlayer(idPlayer,idGroup) then
         table.insert(playerGroup,idGroup)
+    end
+end
+
+function functions.setActiveGroup(idPlayer, idGroup)
+    local playerInfo = functions.getPlayer(idPlayer)
+    if playerInfo ~= nil and functions.isGroupinPlayer(idPlayer, idGroup) then
+        playerInfo["active"] = idGroup
     end
 end
 
@@ -70,41 +87,105 @@ function functions.initTables()
 end
 ---@param player LuaPlayer
 ---@param name string
-function functions.createGroup(player, name)
-    groups = functions.getTableGroup()
+function functions.createGroup(player, name,icon)
+    local groups = functions.getTableGroup()
     if groups ~= nil then
-        configGroup = {
+        local configGroup = {
             name = name,
             owner = player.index,
             recipes = {},
-            surfaces = {}
+            surfaces = {},
+            icon = icon or "iron-gear-wheel",
         }
         
-        id = table.insert(groups,configGroup)
-        print(id)
-        functions.addPlayerInGroup(player.index,#groups)
+        
+        table.insert(groups,configGroup)
+        local idGroup = #groups
+        functions.addPlayerInGroup(player.index,idGroup)
+        functions.setActiveGroup(player.index, idGroup)
     end
+end
+function functions.recipeInGroup(groupID,recipe)
+    local group = functions.getGroup(groupID)
+    if group ~= nil then
+        local founded = false
+        for _, value in pairs(group["recipes"]) do
+            if value["name"] == recipe then
+                founded = true
+            end 
+        end
+        return founded
+    end    
 end
 function functions.addRecipe(groupID,recipe,max,min,priorety)
-    group = functions.getGroup(groupID)
-    print(group)
+    local group = functions.getGroup(groupID)
     if group ~= nil then
-        recipeBody = {
-            name=recipe,
-            rs = {
-                min = min or 0,
-                max = max
-            },
-            blacklist = {},
-            priorety = priorety or 100
-        }
-        table.insert(group["recipes"], recipeBody)
+        if not functions.recipeInGroup(groupID,recipe) then
+            local recipeBody = {
+                name=recipe,
+                rs = {
+                    min = min or 0,
+                    max = max,
+                    enabled = false
+                },
+                priorety = priorety or 100,
+                needed = {},
+                enabled = true,
+                productID = 1
+            }
+            table.insert(group["recipes"], recipeBody)
+            functions.sortRecipes(groupID)
+            return #group["recipes"]
+        end
     end
 end
+
+function functions.addNeeded(groupID, recipeID, item,type,count)
+    local recipe = functions.getRecipe(groupID,recipeID)
+    if recipe ~= nil then
+        for _, need in pairs(recipe["needed"]) do
+            if need["name"] == item then
+                return nil
+            end
+        end
+        local blacklist = {
+            name=item,
+            type=type,
+            count=count,
+        }
+        table.insert(recipe["needed"],blacklist)
+    end
+end
+
 function functions.addRecipePriorety(groupID,recipeID,count)
-    recipe = functions.getRecipe(groupID, recipeID)
+    local recipe = functions.getRecipe(groupID, recipeID)
     if recipe ~= nil then
         recipe["priorety"] = recipe["priorety"] + count
     end
 end
+function functions.sortRecipes(groupID)
+    local group = functions.getGroup(groupID)
+    if group ~= nil then
+        recipes = group["recipes"]
+        table.sort(recipes,function (a, b)
+            pr1 = a["priorety"]
+            pr2 = b["priorety"]
+            return pr1 < pr2
+        end)
+    end
+end
+
+function functions.equals2var(var1,sign, var2)
+    if sign == "<" then
+        return var1 < var2
+    elseif sign == ">" then
+        return var1 > var2
+    elseif sign == "!=" then
+        return var1 ~= var2
+    else
+        return var1 == var2
+    end
+
+end
+
 return functions
